d272b26d3d499a1ccce46ac96627d134e5c120df
heaven-chp <62167253+heaven-chp@users.noreply.github.com>
2022-06-02 16:20:02 +0900

    baekjoon 15649 Go

---
+++ baekjoon/15649/15649.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func dfs(writer *bufio.Writer, visited []bool, result []int, numberChoice, numberEnd, index int) {
+	if index == numberChoice {
+		for _, value := range result {
+			fmt.Fprintf(writer, "%d ", value)
+		}
+		fmt.Fprintf(writer, "\n")
+
+		return
+	}
+
+	for i := 1; i <= numberEnd; i++ {
+		if visited[i] {
+			continue
+		}
+
+		result[index] = i
+
+		visited[i] = true
+		dfs(writer, visited, result, numberChoice, numberEnd, index+1)
+		visited[i] = false
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	N := 0
+	M := 0
+	fmt.Fscanf(reader, "%d %d\n", &N, &M)
+
+	visited := make([]bool, N+1)
+	result := make([]int, M)
+	dfs(writer, visited, result, M, N, 0)
+}
